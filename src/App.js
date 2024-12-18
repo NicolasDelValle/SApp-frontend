a6019fc2c26150f51d63bc53520e3a86e145e943
ideweProject <bruno.lima.taborda@gmail.com>
2024-12-18 20:24:54 -0300

    add login and logout functions with auth0

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">primera vista</div>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import "./i18n";
+import Login from "./components/Login";
+
+import { Auth0Provider } from "@auth0/auth0-react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-hkj76rxtirg2jpfw.us.auth0.com"
+      clientId="owcKfiqfMbfBFcoZuw3Mayf7WznFNPk7"
+      redirectUri={window.location.origin}
+    >
+      <Login />
+    </Auth0Provider>
   </React.StrictMode>
 );
