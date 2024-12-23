928ddc3d626480221d03947b69ca3627e2f04c2e
Nicolas Del Valle <50749255+NicolasDelValle@users.noreply.github.com>
2024-12-23 15:54:53 -0300

    feat: update navbar, get token

--- src/components/Navbar/NavBar.jsx
+++ src/components/Navbar/NavBar.jsx
@@ -1,12 +1,44 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import { NormalButton, DefaultProfile } from "../../components";
-
 import { useAuth0 } from "@auth0/auth0-react";
 
 const NavBar = () => {
-  const { logout, loginWithRedirect, user, isAuthenticated, isLoading } =
-    useAuth0();
-  console.log(user);
+  const {
+    logout,
+    loginWithRedirect,
+    user,
+    isAuthenticated,
+    getAccessTokenSilently,
+  } = useAuth0();
+
+  const [token, setToken] = useState("");
+  console.log(token);
+
+  useEffect(() => {
+    const fetchToken = async () => {
+      try {
+        if (!isAuthenticated || token) return;
+        const fetchedToken = await getAccessTokenSilently({
+          audience: process.env.REACT_APP_AUTH0_AUDIENCE,
+        });
+        setToken(fetchedToken);
+      } catch (error) {
+        console.error("Error al obtener el token de acceso:", error);
+      }
+    };
+
+    fetchToken();
+  }, [isAuthenticated, getAccessTokenSilently, token]);
+
+  const AuthButtons = () =>
+    isAuthenticated ? (
+      <div className="flex items-center gap-2">
+        <DefaultProfile picUrl={user.picture} />
+        <NormalButton danger actionLabel="Logout" action={logout} />
+      </div>
+    ) : (
+      <NormalButton primary actionLabel="Login" action={loginWithRedirect} />
+    );
 
   return (
     <nav className="flex items-center justify-between px-3 drop-shadow-lg bg-white h-14 rounded-xl border border-dark100">
@@ -14,18 +46,7 @@ const NavBar = () => {
         <span className="font-bold text-xl text-goodBlue">SApp</span>
       </div>
       <div>
-        {isAuthenticated ? (
-          <div className="flex items-center justify-between gap-2">
-            <DefaultProfile picUrl={user.picture} />
-            <NormalButton danger actionLabel="Logout" action={() => logout()} />
-          </div>
-        ) : (
-          <NormalButton
-            primary
-            actionLabel="Login"
-            action={() => loginWithRedirect()}
-          />
-        )}
+        <AuthButtons />
       </div>
     </nav>
   );
